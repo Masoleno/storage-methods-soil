de4fd40fe51648473f0b11e8c2f2f37ffef18732
Masoleno <maya.sollen@gmail.com>
2023-06-10 14:31:52 +0100

    Included normality testing and qqplots.

--- RM-anova-pH.R
+++ RM-anova-pH.R
@@ -1,5 +1,8 @@
+#Set numbers to show in standard format instead of scientific notation. 
+#(Reactivate scientific notation with options(scipen=0))
+options(scipen = 999)
 
-# Load libraries
+# Load libraries ----
 
 #library(psych)
 #library(MASS)
@@ -9,14 +12,14 @@ library(rstatix)
 library(car)
 library(MANOVA.RM)
 
-# Read in csv file created at the end of the data-wrangling script
+# Read in csv file created at the end of the data-wrangling script ----
 tidyData <- read.csv("chemistry-data-tidy.csv")
 
 #Look at the data
 head(tidyData)
 str(tidyData)
 
-# Rename columns so they're easier to write.
+# Rename columns so they're easier to write. ----
 tidyData<- tidyData %>% 
   rename(
     "NH4 (mg/kg)" = NH4..mg.kg.,
@@ -30,15 +33,15 @@ tidyData<- tidyData %>%
 
 head(tidyData)
 
-# Convert to factors so that ANOVA and Tukey's work
+# Convert to factors ----
 tidyData$Weeks <- as.factor(tidyData$Weeks)
 
 tidyData$Treatment <- as.factor(tidyData$Treatment)
 
 str(tidyData)
 
-
-# Extract pH data 
+# RM ANOVA for pH ----
+## Extract pH data ----
 pH_data <- tidyData %>%
   select(Sample.ID, pH, Treatment, Weeks)
 
@@ -46,8 +49,64 @@ pH_data <- tidyData %>%
 head(pH_data,20)
 str(pH_data)
 
+## Testing for normality ----
+### Plotting ----
+ggplot(data = pH_data, aes(sample=pH)) +
+  geom_qq() + 
+  geom_qq_line(color="red") + 
+  xlab("Theoretical") +
+  ylab("Sample") +
+  facet_grid(Treatment ~ Weeks)
 
-# Fitting the anova model
+ggplot(data = pH_data, aes(pH)) +
+  geom_histogram() +
+  facet_grid(Treatment ~ Weeks)
+### Shapiro-Wilks ----
+normality<-pH_data %>%
+  group_by(Treatment,Weeks) %>%
+  shapiro_test(pH)
+data.frame(normality)
+
+## Fitting anova model with rstatix package ----
+pH_mod <- anova_test(data = pH_data, dv = pH, wid = Sample.ID, within = c(Treatment, Weeks))
+get_anova_table(pH_mod) #the get_anova_table function (rstatix) automatically applies "Greenhouse-Geisser sphericity 
+#correction" on any factors that violate this assumption
+
+### Post-hoc tests ----
+pH.one.way <- pH_data %>%
+  group_by(Treatment) %>%
+  anova_test(dv = pH, wid = Sample.ID, within = Weeks) %>%
+  get_anova_table() %>%
+  adjust_pvalue(method = "bonferroni")
+pH.one.way
+
+pH.one.way2 <- pH_data %>%
+  group_by(Weeks) %>%
+  anova_test(dv = pH, wid = Sample.ID, within = Treatment) %>%
+  get_anova_table() %>%
+  adjust_pvalue(method = "bonferroni")
+pH.one.way2
+
+### Pairwise comparisons between treatment groups ----
+pwc <- pH_data %>%
+  group_by(Weeks) %>%
+  pairwise_t_test(
+    pH ~ Treatment, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+pwc
+
+### Pairwise comparisons between time points ----
+pwc2 <- pH_data %>%
+  group_by(Treatment) %>%
+  pairwise_t_test(
+    pH ~ Weeks, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+pwc2
+
+
+## Fitting the anova model using aov ----
 model.aov <- aov(pH ~ Treatment*Weeks + Error(Sample.ID/(Treatment*Weeks)), data = pH_data)
 summary(model.aov)
 
